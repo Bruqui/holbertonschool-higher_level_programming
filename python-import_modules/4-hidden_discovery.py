f9a7e976bd11791a9c74668b5631e3981ba28db6
root <dylan2003gd@gmail.com>
2024-09-04 10:05:15 +0000

    4. Who are you?

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 import hidden_4
 
 if __name__ == '__main__':
-    for name in dir(hidden_4):
-        if name[:2] != "__":
+    names = dir(hidden_4)
+    for name in sorted(names):
+        if not name.startswith('__'):
             print(name)
