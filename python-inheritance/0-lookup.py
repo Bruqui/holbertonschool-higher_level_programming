e560325a4ab466cbb34f2a3e3dc112b23437e0ae
Dylan Gros Desormeaux <dylan2003gd@gmail.com>
2024-09-23 11:05:30 +0200

    0. Lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module provides a function that returns a list of attributes and methods
+of an object
+"""
+
+
 def lookup(obj):
     """
     Return the list of available attributes and methods of an object.
