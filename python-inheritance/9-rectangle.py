7fc99641d7b49f93335bcabb9f138b425e4c17e5
Bruqui <dylan2003gd@gmail.com>
2024-09-24 13:35:21 +0200

    9. Full rectangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module that defines a Rectangle class inheriting from BaseGeometry.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    A class representing a rectangle, inheriting from BaseGeometry.
+    """
+    def __init__(self, width, height):
+        """
+        Initialize a new Rectangle instance.
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        Both width and height are validated to be positive integers.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Calculates the area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Returns the string representation of the rectangle.
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
