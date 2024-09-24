9fe1ccf83772f3269663118399fd6e48aebf6363
Bruqui <dylan2003gd@gmail.com>
2024-09-24 16:57:36 +0200

    1. Shapes, Interfaces, and Duck Typing

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -2,6 +2,7 @@
 from abc import ABC, abstractmethod
 import math
 
+
 class Shape(ABC):
     """
     Abstract base class for shapes.
@@ -16,6 +17,7 @@ class Shape(ABC):
             float: The area of the shape.
         """
         pass
+
     @abstractmethod
     def perimeter(self):
         """
@@ -24,6 +26,7 @@ class Shape(ABC):
             float: The perimeter of the shape.
         """
         pass
+
 
 class Circle(Shape):
     """
@@ -39,7 +42,7 @@ class Circle(Shape):
             radius (float): The radius of the circle.
         """
         self.radius = radius
-    
+
     def area(self):
         """
         Calculate the area of the circle.
@@ -47,7 +50,7 @@ class Circle(Shape):
             float: The area of the circle.
         """
         return math.pi * self.radius ** 2
-    
+
     def perimeter(self):
         """
         Calculate the perimeter (circumference) of the circle.
@@ -55,7 +58,7 @@ class Circle(Shape):
             float: The perimeter of the circle.
         """
         return 2 * math.pi * self.radius
-    
+
 class Rectangle(Shape):
     """
     Rectangle class that represents a rectangle.
@@ -73,7 +76,7 @@ class Rectangle(Shape):
         """
         self.width = width
         self.height = height
-    
+
     def area(self):
         """
         Calculate the area of the rectangle.
@@ -81,7 +84,7 @@ class Rectangle(Shape):
             float: The area of the rectangle.
         """
         return self.width * self.height
-    
+
     def perimeter(self):
         """
         Calculate the perimeter of the rectangle.
@@ -89,7 +92,7 @@ class Rectangle(Shape):
             float: The perimeter of the rectangle.
         """
         return 2 * (self.width + self.height)
-    
+
 def shape_info(shape):
     """
     Print the area and perimeter of a given shape object using duck typing.
