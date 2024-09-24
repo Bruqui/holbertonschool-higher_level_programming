3ed27afcec0e4ef5c388cb11fe7bfbdbbb6c0748
Bruqui <dylan2003gd@gmail.com>
2024-09-24 13:24:10 +0200

    7. Integer validator

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module that defines a class BaseGeometry with methods for geometry 
+Module that defines a class BaseGeometry with methods for geometry
 calculations.
 """
 
