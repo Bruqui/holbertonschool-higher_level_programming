762dea4700f51d86ac6e01b636285813530e8771
Bruqui <dylan2003gd@gmail.com>
2024-10-08 10:36:55 +0200

    2. Consuming and processing data from an API using Python

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -4,40 +4,27 @@ import csv
 
 def fetch_and_print_posts():
     """
-    Fetches posts from the JSONPlaceholder API and prints their titles.
-    This function sends a GET request to the JSONPlaceholder API to retrieve
-    posts. It prints the  HTTP status code to indicate the success or failure
-    of the request. If successful (status code 200), it parses the JSON
-    response and iterates over the posts, printing each post's title.
+    Fetches posts from a url and prints the title of each post
     """
-    response = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print('Status Code: {}'.format(response.status_code))
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
-            print(post['title'])
-    else:
-        print('Failed to fetch posts.')
+            print(f"Title: {post['title']}")
 
 
 def fetch_and_save_posts():
     """
-    Fetches posts from the JSONPlaceholder API and saves them to a CSV file.
-    This function sends a GET request to the JSONPlaceholder API to retrieve
-    posts. If the request is successful (status code 200), it parses the JSON
-    data and extracts the post ID, title, and body. The extracted data is
-    written into a CSV file named 'posts.csv', with each post as a row in
-    the file. The CSV contains three columns: 'id', 'title', and 'body'.
+    Fetches posts from a URL and saves them into a CSV file
     """
-    response = requests.get('https://jsonplaceholder.typicode.com/posts')
-    if response.status_code == 200:
-        posts = response.json()
-        csv_data = [{'id': post['id'], 'title': post['title'],
-                     'body': post['body']} for post in posts]
-        with open('posts.csv', mode='w', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
-            writer.writeheader()
-            writer.writerow(csv_data)
-        print('Posts have been saved to posts.csv.')
-    else:
-        print('Failed to fetch posts.')
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    posts = response.json()
+    with open('posts.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        fields = ['id', 'title', 'body']
+        writer.writerow(fields)
+        for post in posts:
+            writer.writerow([post['id'], post['title'], post['body']])
