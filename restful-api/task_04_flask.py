589f7e6d36140e439c1a28a60dc3f182b79428cc
Bruqui <dylan2003gd@gmail.com>
2024-10-10 10:48:03 +0200

    4. Develop a Simple API using Python with Flask

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,11 +1,10 @@
 from flask import Flask, jsonify, request
 
+
 app = Flask(__name__)
 
-users = {
-    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
-    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
-}
+
+users = {}
 
 
 @app.route('/')
@@ -19,7 +18,7 @@ def status():
 
 
 @app.route('/data')
-def get_data():
+def get_usernames():
     return jsonify(list(users.keys()))
 
 
@@ -35,11 +34,19 @@ def get_user(username):
 @app.route('/add_user', methods=['POST'])
 def add_user():
     user_data = request.get_json()
-    username = user_data.get("username")
-    if not username:
+    if "username" not in user_data:
         return jsonify({"error": "Username is required"}), 400
-    users[username] = user_data
-    return jsonify({"message": "User added", "user": user_data}), 201
+    username = user_data["username"]
+    users[username] = {
+        "username": username,
+        "name": user_data.get("name"),
+        "age": user_data.get("age"),
+        "city": user_data.get("city")
+    }
+    return jsonify({
+        "message": "User added",
+        "user": users[username]
+    })
 
 
 if __name__ == "__main__":
