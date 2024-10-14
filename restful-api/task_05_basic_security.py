a8e44e80050e1e34d4e1abb3ccd14f84711feaec
Bruqui <dylan2003gd@gmail.com>
2024-10-14 10:42:53 +0200

    5. API Security and Authentication Techniques

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -7,8 +5,7 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 
 
 app = Flask(__name__)
-
-
+app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
 
 
@@ -32,7 +29,7 @@ def verify_password(username, password):
 @app.route('/bassic-protected')
 @auth.login_required
 def basic_protected():
-    return jsonify({"message": "Basic Auth: Access Granted"}), 200
+    return "Basic Auth: Access Granted"
 
 
 @app.route('/login', methods=['POST'])
@@ -41,22 +38,19 @@ def login():
     username = data.get('username')
     password = data.get('password')
     user = users.get(username)
-
     if not user or not check_password_hash(user['password'], password):
-        return jsonify({"error": "Invalid credentials"}), 401
-    
-    access_token = create_access_token(identity={"username": username, "role": user['role']})
-    return jsonify(access_token=access_token), 200
+        access_token = create_access_token(identity={"username": username, "role": user['role']})
+        return jsonify(access_token=access_token)
+    return jsonify({"error": "Invalid credentials"}), 401
 
 
-@app.route('/jwt-protected', methods=['GET'])
+@app.route('/jwt-protected')
 @jwt_required()
 def jwt_protected():
-    current_user = get_jwt_identity()
     return jsonify({"message": "JWT Auth: Access Granted", "user": current_user}), 200
 
 
-@app.route('/admin-only', methods=['GET'])
+@app.route('/admin-only')
 @jwt_required()
 def admin_only():
     current_user = get_jwt_identity()
@@ -69,16 +63,21 @@ def admin_only():
 def handle_unauthorized_error(err):
     return jsonify({"error": "Missing or invalid token"}), 401
 
-
 @jwt.invalid_token_loader
 def handle_invalid_token_error(err):
     return jsonify({"error": "Invalid token"}), 401
-
 
 @jwt.expired_token_loader
 def handle_expired_token_error(err):
     return jsonify({"error": "Token has expired"}), 401
 
+@jwt.revoked_token_loader
+def handle_revoked_token_error(err):
+    return jsonify({"error": "Token has been revoked"}), 401
+
+@jwt.needs_fresh_token_loader
+def handle_needs_fresh_token_error(err):
+    return jsonify({"error": "Fresh token required"}), 401
 
 if __name__ == '__main__':
     app.run()
