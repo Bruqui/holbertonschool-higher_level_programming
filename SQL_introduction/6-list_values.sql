6d0cf76138a56baab42d39a783ac7834fee391d5
root <dylan2003gd@gmail.com>
2024-10-29 18:06:23 +0000

    6. List all in table

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- Retrieves all records from the 'first_table' table.
+-- The '*' wildcard selects all columns in the table, returning every row and all column data.
+-- This command is useful for viewing the entire contents of 'first_table' for review or analysis.
+
+SELECT * FROM first_table;
