d8479a3d37f210a11b9c0a4bef41dea49f59f247
root <dylan2003gd@gmail.com>
2024-10-29 18:16:30 +0000

    8. Count 89

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- Counts the number of records in 'first_table' where the 'id' column is equal to 89.
+-- This command is useful for checking if a specific record with 'id' = 89 exists
+-- and can help in verifying successful inserts or checking for duplicates.
+
+SELECT COUNT(*) FROM first_table WHERE id = 89;
