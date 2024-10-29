4eabb198cf1ba5e6e7ccbc5ed9be2c5097ef6a5f
root <dylan2003gd@gmail.com>
2024-10-29 11:20:35 +0000

    2. Delete a database

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- Drops (deletes) the database named 'hbtn_0c_0' if it exists.
+-- The 'IF EXISTS' clause prevents an error if the database does not exist,
+-- ensuring that the command runs smoothly whether or not the database is present.
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
