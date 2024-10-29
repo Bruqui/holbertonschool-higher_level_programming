560022aaa6f97e358e39e64ae6a0f10e7ca898c7
root <dylan2003gd@gmail.com>
2024-10-29 20:02:58 +0000

    17. Go to UTF8

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,19 +1,14 @@
--- Changes the default character set and collation for the database 'hbtn_0c_0'.
--- This command sets the character set to 'utf8mb4', which supports a wider range of Unicode characters,
--- and the collation to 'utf8mb4_unicode_ci', which ensures case-insensitive comparisons.
+-- Alters the character set and collation of the database 'hbtn_0c_0'.
+-- This sets the character set to 'utf8mb4', which supports a wider range of characters (including emojis).
+-- The collation 'utf8mb4_unicode_ci' allows for case-insensitive comparisons based on Unicode standards.
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+-- Selects the database 'hbtn_0c_0' for use in subsequent commands.
+-- This command ensures that the following commands operate on the correct database context.
+USE hbtn_0c_0;
 
--- Converts the 'first_table' to use the 'utf8mb4' character set and 'utf8mb4_unicode_ci' collation.
--- This ensures that all existing text data in the table will be stored using the specified character set,
--- allowing for better support for multi-language text and special characters.
-
+-- Alters the 'first_table' to convert its character set and collation to match the database settings.
+-- This ensures that all textual data in 'first_table' will support the same character set and collation,
+-- allowing for consistent data handling and compatibility with various characters.
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Modifies the 'name' column in 'first_table' to explicitly use the 'utf8mb4' character set and 
--- 'utf8mb4_unicode_ci' collation.
--- This ensures that any new data inserted into the 'name' column will be compatible with the 
--- specified character set and collation, allowing for proper handling of Unicode characters.
-
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL;
 
