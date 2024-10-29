5d79eb1f4220c09e87e8c839d6347e220fdfb257
root <dylan2003gd@gmail.com>
2024-10-29 11:24:51 +0000

    3. List tables

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- Displays a list of all tables in the currently selected database.
+-- This command is useful for reviewing the structure of the database 
+-- and understanding what data is available for querying.
+
+SHOW TABLES;
