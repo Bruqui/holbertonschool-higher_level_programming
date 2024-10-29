222936d95181857ed6da20d31dac2707c550c2ac
root <dylan2003gd@gmail.com>
2024-10-29 18:13:19 +0000

    7. First add

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,1 +1,5 @@
-INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+-- Inserts a new record with 'id' = 89 and 'name' = 'Best School' into the 'first_table' table.
+-- The 'IGNORE' keyword prevents errors if a record with the same 'id' already exists in the table,
+-- allowing the insertion to continue without affecting existing data.
+
+INSERT IGNORE INTO first_table (id, name) VALUES (89, 'Best School');
