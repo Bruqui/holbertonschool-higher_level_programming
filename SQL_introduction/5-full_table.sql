9770d54093c7462cf538a20805d40c4848fecd12
root <dylan2003gd@gmail.com>
2024-10-29 18:01:58 +0000

    5. Full description

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- Displays the SQL statement that was used to create the 'first_table' table.
+-- This command is useful for reviewing the exact structure and constraints
+-- applied to 'first_table', including column definitions, data types, and any indexes.
+
+SHOW CREATE TABLE first_table;
