ea669259037f97fbebb7901c879dc1ddcf2504c6
root <dylan2003gd@gmail.com>
2024-10-29 11:14:15 +0000

    1. Create a database

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- Creates a new database named 'hbtn_0c_0' if it doesn't already exist.
+-- The 'IF NOT EXISTS' clause prevents errors if the database is already present,
+-- ensuring the command only creates the database if it is missing.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
