f0c169d1874654383ad1876fe6140b93d3261105
Bruqui <dylan2003gd@gmail.com>
2024-10-30 15:01:07 +0100

    13. Number of shows by genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,9 +4,8 @@
 -- Filters results to show only genres that have at least one associated show.
 -- Orders results in descending order based on the number of shows for each genre.
 
-SELECT genres_name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM genres
-LEFT JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
-HAVING COUNT(tv_show_genres.show_id) > 0
-ORDER BY number_of_shows DESC;
+SELECT tv_genres_name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_show_genres
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC, tv_genres.id ASC;
