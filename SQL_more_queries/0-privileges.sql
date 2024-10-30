3fc613f45eceeec4fec54b9b24952d652968199a
root <dylan2003gd@gmail.com>
2024-10-30 10:18:54 +0000

    0. My privileges!

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,9 @@
+-- Displays the privileges (grants) assigned to 'user_0d_1' when connecting from 'localhost'.
+-- This command helps in identifying what actions 'user_0d_1' is allowed to perform in the database.
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Similarly, displays the privileges for 'user_0d_2' when connecting from 'localhost'.
+-- This command helps verify or audit the permissions assigned to 'user_0d_2'.
+
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
