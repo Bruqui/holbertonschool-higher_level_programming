c7b6f5061f1a5bb53d0fb4e80426f6035e18062e
root <dylan2003gd@gmail.com>
2024-10-30 13:02:37 +0000

    8. Cities of California

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- Selects all columns from the 'cities' table.
+-- Filters the results to include only cities that belong to the state named 'California'.
+-- The state is identified by its id, which is retrieved from the 'states' table.
+-- The results are ordered by the 'id' column in ascending order.
+
+SELECT *
+FROM cities
+WHERE states_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC;
