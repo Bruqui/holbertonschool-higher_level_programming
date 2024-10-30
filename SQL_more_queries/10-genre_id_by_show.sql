cd12b9014d770bf2f14a685e163d41521ad75bec
root <dylan2003gd@gmail.com>
2024-10-30 13:23:41 +0000

    10. Genre ID by show

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- Retrieves each TV show's title and its associated genre ID.
+-- Joins 'tv_shows' with 'tv_show_genres' on matching TV show IDs to link each show to its genres.
+-- Orders results alphabetically by title and, for each title, by genre ID in ascending order.
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
