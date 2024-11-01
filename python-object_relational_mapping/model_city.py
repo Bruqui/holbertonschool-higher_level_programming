068ad35ac1cef375320b72638488706de5ac523e
Bruqui <dylan2003gd@gmail.com>
2024-11-01 19:18:34 +0100

    14. Cities in state

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -3,9 +3,8 @@
 This module defines a City class that is mapped to the cities table in a MySQL database.
 """
 
+from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from .model_state import Base
 
 
 class City(Base):
@@ -18,5 +17,3 @@ class City(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-    state = relationship("State", back_populates="cities")
--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,26 +4,20 @@ Defines the State class and a Base instance to map to the states table
 in a MySQL database.
 """
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from .model_city import City
 
-Base = declarative_base()
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
     """
-    State class definition to map to the states table in the MySQL database.
-
-    Attributes:
-        id (int): Auto-generated, unique integer, primary key, non-null.
-        name (str): String column with max 128 characters, non-null.
+    Class State; instance of Base
+    Linked to MySQL table "states"
     """
 
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
-    cities = relationship("City", back_populates="state", cascade="all, delete-orphan")
