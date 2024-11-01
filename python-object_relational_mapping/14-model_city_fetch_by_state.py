d119fe8bb4b39a214237fb5c77a81bea5dee9f2e
Bruqui <dylan2003gd@gmail.com>
2024-11-01 19:07:48 +0100

    14. Cities in state

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -6,7 +6,6 @@ Fetches and prints all City objects from the database hbtn_0e_14_usa.
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from .model_state import Base, State
 from .model_city import City
 
 
